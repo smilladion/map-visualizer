41ee5943c9440598f9c972e1b297c329b390732a
lippITU <lipp@itu.dk>
2020-02-27 12:59:59 +0100

    'clean' slate

--- src/main/java/bfst20/tegneprogram/Drawable.java
+++
@@ -1,9 +0,0 @@
-package bfst20.tegneprogram;
-
-import javafx.scene.canvas.GraphicsContext;
-
-public interface Drawable {
-    void draw(GraphicsContext gc);
-
-    Type getType();
-}
--- src/main/java/bfst20/tegneprogram/OSMRelation.java
+++
@@ -1,7 +0,0 @@
-package bfst20.tegneprogram;
-
-import java.util.ArrayList;
-
-public class OSMRelation extends ArrayList<OSMWay> {
-    private static final long serialVersionUID = -855030698315224615L;
-}
