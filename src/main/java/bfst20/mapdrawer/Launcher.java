2626604c710e3478af264c5cc1666a1eeee26234
lippITU <lipp@itu.dk>
2020-02-27 13:23:03 +0100

    add Main.java and Launcher.java

---
+++ src/main/java/bfst20/mapdrawer/Launcher.java
@@ -0,0 +1,17 @@
+package bfst20.mapdrawer;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        System.out.println("herp it works");
+    }
+
+	public static void main(String[] args) {
+        launch(args);
+	}
+}
---
+++ src/main/java/bfst20/mapdrawer/Main.java
@@ -0,0 +1,7 @@
+package bfst20.mapdrawer;
+
+public class Main {
+    public static void main(String[] args) {
+        Launcher.main(args);
+    }
+}
