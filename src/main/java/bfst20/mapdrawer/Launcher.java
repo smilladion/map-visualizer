ecd6f74ae18519d039f0569ffda429f34f55d31a
Smilla <smdi@itu.dk>
2020-02-29 16:38:19 +0100

    Add empty FX window

--- src/main/java/bfst20/mapdrawer/Launcher.java
+++ src/main/java/bfst20/mapdrawer/Launcher.java
@@ -1,7 +1,11 @@
 package bfst20.mapdrawer;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class Launcher extends Application {
 
@@ -11,6 +15,11 @@ public class Launcher extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        System.out.println("herp it works");
+        primaryStage.setTitle("Google Map'nt");
+        TextArea label = new TextArea("Hello World!");
+        StackPane root = new StackPane();
+        Scene primaryScene = new Scene(root, 1000, 800);
+        primaryStage.setScene(primaryScene);
+        primaryStage.show();
     }
 }
