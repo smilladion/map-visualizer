fca12c3c9505f08533d34260a51d6970292a200d
Kirstine Berg Jørgensen <kbjo@itu.dk>
2020-03-02 15:50:50 +0100

    textfield with search text added

--- src/main/java/bfst20/mapdrawer/View.java
+++ src/main/java/bfst20/mapdrawer/View.java
@@ -2,6 +2,8 @@ package bfst20.mapdrawer;
 
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -9,8 +11,11 @@ public class View {
 
     public View(Stage primaryStage) {
         primaryStage.setTitle("Google Map'nt");
+        TextField textField = new TextField("Search here...");
+        HBox hbox = new HBox(textField);
         Canvas canvas = new Canvas(640, 480);
         StackPane root = new StackPane(canvas);
+        root.getChildren().add(hbox);
         Scene primaryScene = new Scene(root);
         primaryStage.setScene(primaryScene);
         primaryStage.show();
