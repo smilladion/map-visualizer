0a3a97613d137eee102de6f4b7a480f41800e9af
Smilla <smdi@itu.dk>
2020-03-08 16:10:02 +0100

    Fix OSM connections

--- src/main/java/bfst20/mapdrawer/osm/OSMNode.java
+++ src/main/java/bfst20/mapdrawer/osm/OSMNode.java
@@ -1,21 +1,17 @@
-package bfst20.mapdrawer;
+package bfst20.mapdrawer.osm;
 
 import java.util.function.LongSupplier;
 
 public class OSMNode implements LongSupplier {
-    float lat;
-    float lon;
-    long id;
+
+    private final float lat;
+    private final float lon;
+    private final long id;
 
     public OSMNode(float lon, float lat, long id) {
         this.lon = lon;
         this.lat = lat;
         this.id = id;
-    }
-
-    @Override
-    public long getAsLong() {
-        return id;
     }
 
     public float getLat() {
@@ -24,5 +20,10 @@ public class OSMNode implements LongSupplier {
 
     public float getLon() {
         return lon;
+    }
+
+    @Override
+    public long getAsLong() {
+        return id;
     }
 }
