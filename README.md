bb5f95bb24e213bc129ce9ae6f8050cd2ef7ca51
Smilla <smdi@itu.dk>
2020-05-02 15:46:42 +0200

    Cleaned up from jar creation

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # 👩‍💻 Førsteårsprojekt: Danmarkskort 👨‍💻
 Repositoriet til Gruppe 19.
+
+OBS: Da vores program bruger mere ram end de fleste computere allokerer automatisk (ca. 4 GB 
+bliver brugt), så skal .jar-filen køres fra kommandolinjen ved at skrive: 
+
+java -Xms1G -Xmx5G -jar BFST20Gruppe19.jar
+
+Den tager omkring 30-40 sekunder at åbne. Hvis det stadig ikke skulle virke, allokér evt. mere ram, og ellers
+ kan selve projektet også køres med gradlew run (her åbnes i stedet et kort af Samsø ved start).
--- src/main/java/bfst20/mapdrawer/LauncherFX.java
+++ src/main/java/bfst20/mapdrawer/LauncherFX.java
@@ -5,6 +5,8 @@ import bfst20.mapdrawer.osm.OSMMap;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.io.File;
+
 /**
  * Class to launch our program through JavaFX.
  */
@@ -21,14 +23,15 @@ public class LauncherFX extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception {
         long start = System.currentTimeMillis();
         System.out.println("Loading map...");
 
-        new MapView(
-                OSMMap.loadBinary(LauncherFX.class.getResourceAsStream("/denmark.bin")),
-                primaryStage
-        );
+        File file = new File("src/main/resources/samsoe.osm");
+
+        if (OSMMap.fromFile(file) != null) {
+            new MapView(OSMMap.fromFile(file), primaryStage);
+        }
 
         System.out.println("Map loaded in " + (System.currentTimeMillis() - start) / 1000 + "s");
     }
